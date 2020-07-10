3019211b40b5bc6e40e4f5d7708e696241779065
Felix Münscher <Felix.Muenscher@mni.thm.de>
2020-07-10 15:43:25 +0200

    Bot layer is working

--- src/main/scala/telegram/GrannyFrameBot.scala
+++ src/main/scala/telegram/GrannyFrameBot.scala
@@ -6,10 +6,12 @@ import com.bot4s.telegram.api.RequestHandler
 import com.bot4s.telegram.api.declarative.Commands
 import com.bot4s.telegram.clients.{FutureSttpClient, ScalajHttpClient}
 import com.bot4s.telegram.future.{Polling, TelegramBot}
+import com.bot4s.telegram.methods.GetFile
+import com.bot4s.telegram.models.Message
 import com.softwaremill.sttp.okhttp.OkHttpFutureBackend
 import slogging.{LogLevel, LoggerConfig, PrintLoggerFactory}
 
-import scala.util.Try
+import scala.util.{Success, Try}
 import scala.concurrent.Future
 
 class GrannyFrameBot(val token: String) extends TelegramBot
@@ -27,8 +29,37 @@ class GrannyFrameBot(val token: String) extends TelegramBot
     reply("Welcome to the GrannyFrame Bot").void
   }
 
+  onMessage { implicit msg =>
+    if (msg.photo.isEmpty) {
+      reply("This bot can only recieve Pictures")
+    }
+    using(_.photo) { photo =>
+      request(GetFile(photo.last.fileId)).andThen({
+        case Success(file) =>
+          file.filePath match {
+
+            case Some(filePath) =>
+              val url = s"https://api.telegram.org/file/bot${token}/${filePath}"
+
+              for {
+                res <- Future {
+                  scalaj.http.Http(url).asBytes
+                }
+                if res.isSuccess
+                bytes = res.body
+                _ <- reply(s"File with ${bytes.size} bytes received.").void
+              } yield ()
+            case None =>
+              reply("No file_path was returned").void
+          }
+      }).void
+    }
+
+  }
+
   // Int(n) extractor
   object Int {
     def unapply(s: String): Option[Int] = Try(s.toInt).toOption
   }
+
 }
