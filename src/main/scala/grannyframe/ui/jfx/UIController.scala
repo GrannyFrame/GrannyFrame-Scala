4e17f63e4843d8e5a0717837be2fca5913c138fc
Felix Münscher <Felix.Muenscher@mni.thm.de>
2020-08-06 21:12:30 +0200

    Fixes Scaling

--- src/main/scala/grannyframe/ui/jfx/UIController.scala
+++ src/main/scala/grannyframe/ui/jfx/UIController.scala
@@ -1,31 +1,37 @@
 package grannyframe.ui.jfx
 
-import java.nio.file.{Files, Paths}
-
 import grannyframe.persistence.ImageEntity
 import grannyframe.ui.jfx.views.{ImageViewer, LoadingViewer}
 import javafx.scene.Scene
-import javafx.scene.layout.StackPane
+import javafx.scene.layout.{AnchorPane, StackPane}
 import javafx.stage.Stage
 
 class UIController(val stage: Stage) {
   stage.setTitle("GrannyFrame")
-  val frame = new StackPane()
-  val scene = new Scene(frame)
+  val frame = new AnchorPane()
+  val scene = new Scene(frame, 800, 600)
   stage.setScene(scene)
   stage.setFullScreen(true)
   stage.show()
 
-  val viewer = new ImageViewer();
+  val viewer = new ImageViewer()
 
   def showSplash(msg: String): Unit ={
     val splash = new LoadingViewer(msg)
+    AnchorPane.setTopAnchor(splash, 0f)
+    AnchorPane.setRightAnchor(splash, 0f)
+    AnchorPane.setBottomAnchor(splash, 0f)
+    AnchorPane.setLeftAnchor(splash, 0f)
     frame.getChildren.clear()
     frame.getChildren.add(splash)
   }
 
-  def initImage(): Unit ={
+  def initImage(): Unit = {
     frame.getChildren.clear()
+    AnchorPane.setTopAnchor(viewer, 0f)
+    AnchorPane.setRightAnchor(viewer, 0f)
+    AnchorPane.setBottomAnchor(viewer, 0f)
+    AnchorPane.setLeftAnchor(viewer, 0f)
     frame.getChildren.add(viewer)
   }
 
