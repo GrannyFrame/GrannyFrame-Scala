a50e098b410756eb8f63bab85fec0b843c8f0cbf
Felix Münscher <Felix.Muenscher@mni.thm.de>
2020-09-05 15:58:39 +0200

    Adds Ruby Updater and Launcher Script

---
+++ launcher.rb
@@ -0,0 +1,46 @@
+#!/bin/ruby
+
+require 'json'
+require 'httparty'
+
+ENV['TELEGRAM_TOKEN'] = ""
+PROG_DIR = "GrannyFrame-Scala"
+TEMP_FILE = "grannyframe_latest.tgz"
+VERSION_FILE = "version.txt"
+curr_vers = nil
+latest_vers = nil
+tarball_url = nil
+
+puts "Starting GrannyFrame Launcher..."
+
+if File.file? VERSION_FILE
+  curr_vers = File.read(VERSION_FILE)
+end
+
+puts "Currently installed version: #{curr_vers ? curr_vers : 'NONE!'}"
+
+response = HTTParty.get("https://api.github.com/repos/GrannyFrame/GrannyFrame-Scala/releases/latest")
+
+if response.code == 200
+  json = JSON.parse response.body
+  latest_vers = json["tag_name"]
+  tarball_url = json["assets"][0]["browser_download_url"]
+  puts "Latest Release:"
+  puts "Tag: #{latest_vers}"
+else
+  abort "Error requesting GitHub... Is there an internet Connection?"
+end
+
+if curr_vers != latest_vers
+  puts "Updating to latest version #{latest_vers} ..."
+  `rm -rf #{PROG_DIR}`
+  `wget --max-redirect=5 #{tarball_url} -O #{TEMP_FILE}`
+  `mkdir #{PROG_DIR}`
+  `tar -xzvf #{TEMP_FILE} --strip 1 --directory ./#{PROG_DIR}`
+  `rm #{TEMP_FILE}`
+  File.write(VERSION_FILE, latest_vers)
+end
+
+puts "Launching GrannyFrame..."
+pid = spawn("./GrannyFrame-Scala/bin/grannyframescala")
+Process.wait pid
